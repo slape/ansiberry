17e14f575c3b1d2440dc503cb05eb4cfa6bd699d
Isaac Slape <sslape@2u.com>
2020-03-22 13:16:43 -0400

    change path to dashboard provisioning json

--- provisioners/roles/SpeedMonitor/files/configure_influx.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# configure influx db
-
-influx
-CREATE DATABASE internetspeed
-CREATE USER "speedmonitor" WITH PASSWORD 'pimylifeup'
-GRANT ALL ON "internetspeed" to "speedmonitor"
-quit
